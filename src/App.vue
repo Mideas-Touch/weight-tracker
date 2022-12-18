7bde42bd3f606dc172750c43360a57bd82aa3f77
thrasher <thrasher@viper.moringa>
2022-12-17 23:29:45 -0600

    setup variables

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <script setup>
+import { ref, shallowRef, computed, watch, nexttick} from 'vue'
+import Chart from 'chart.js/auto'
+
+const weights = ref([])
+
+const weightChartEl = ref(null)
+
+const weightChart = shallowRef(null)
+
+const weightInput = ref(60.0)
+
+const currentWeight = computed(() =>{
+  return weights.value.sort((a, b) => b.date - a.date)[0] || { weight: 0}
+
+  const addWeight = () =>{
+    weights.value.push({
+      weight: weightInput.value,
+      date: new Date().getTime()
+    })
+  }
+})
+
 </script>
 
 <template>
-  <h1>Hello, world!</h1>
+  <main>
+    <h1>Weight Tracker</h1>
+  </main>
 </template>
 
 <style scoped>
